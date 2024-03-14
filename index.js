0207f0b49c8d5ee37bc929d9d1b38813e954bf81
JohnMutemi <jmtutorsalp@gmail.com>
2024-03-15 02:57:21 +0300

    modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats =["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(string) {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(string) {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(string) {
+    return cats.concat("Broom");
+}
+
+function prependCat(string) {
+    return ["Arnold"].concat(cats);
+}
+
+
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
